5e4c8e22b37506495294f589c8bec4c90f327762
Pedro Luiz <pedroluizlsg@gmail.com>
2022-11-04 18:22:22 -0300

    Req 2 - finalizado - procura por um id específico na rota

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs').promises;
 const path = require('path');
 const bodyParser = require('body-parser');
+const { takeCoverage } = require('v8');
 
 const app = express();
 app.use(bodyParser.json());
@@ -20,6 +21,15 @@ app.get('/talker', async (_req, res) => {
   const talkers = await fs.readFile(talkerFile, 'utf-8');
   const response = talkers ? JSON.parse(talkers) : [];
   res.status(200).json(response);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const findThisId = req.params.id;
+  const talkers = await fs.readFile(talkerFile, 'utf-8');
+  const talkersJson = JSON.parse(talkers);
+  const lookingForId = talkersJson.filter((talker) => talker.id === Number(findThisId));
+  if (lookingForId.length) res.status(200).json(lookingForId[0]);
+  else res.status(404).json({ message: "Pessoa palestrante não encontrada" });
 });
 
 app.listen(PORT, () => {
